30a54cbd425d0a221de78fe6cbc85be3d2fb17d9
Michal Harrison <mlh1477@gmail.com>
2020-01-23 08:46:40 -0500

    Video 3 NGRX Store-Devtools Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { SharedModule } from "./shared/shared.module";
 import { ProductsModule } from "./products/products.module";
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -29,7 +31,8 @@ import { reducers, metaReducers } from './reducers';
         strictStateImmutability: true,
         strictActionImmutability: true
       }
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
