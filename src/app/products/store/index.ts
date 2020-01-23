c4db23a1e8c3306fd2900e10da1afc3bef0b1b03
Michal Harrison <mlh1477@gmail.com>
2020-01-23 11:15:56 -0500

    Video 4 NGRX Store - Create Reducer

--- src/app/products/store/index.ts
+++ src/app/products/store/index.ts
@@ -3,19 +3,41 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../../environments/environment';
+  MetaReducer,
+  createReducer,
+  on
+} from "@ngrx/store";
+import { environment } from "../../../environments/environment";
+import { Product } from "../models/product";
+import { loadProductsSuccess, loadProductsFailure } from "./product.actions";
 
-export const productStateFeatureKey = 'productState';
+export const productStateFeatureKey = "productState";
 
 export interface ProductState {
-
+  products: Product[];
+  error: any;
 }
 
-export const reducers: ActionReducerMap<ProductState> = {
-
+export const initialState: ProductState = {
+  products: undefined,
+  error: undefined
 };
 
+export const reducers = createReducer(
+  initialState,
+  on(loadProductsSuccess, (state, action) => {
+    return {
+      products: action.products
+    };
+  }),
+  on(loadProductsFailure, (state, action) => {
+    return {
+      products: state.products,
+      error: action.error
+    };
+  })
+);
 
-export const metaReducers: MetaReducer<ProductState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<ProductState>[] = !environment.production
+  ? []
+  : [];
